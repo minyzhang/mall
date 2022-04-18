a081b3020788f4f985ea1e900d5023544dbed980
Miny Zhang <miny.zhang@siklsoftware.com>
2022-04-18 16:46:47 +0800

    axios request data

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,35 +1,72 @@
-import React from "react";
-import { Table } from "antd";
+import React, { Fragment, useState } from "react";
+import { Table, Button, Modal } from "antd";
+import axios from "axios";
 
 const columns = [
   {
-    title: 'github名字',
-    dataIndex: 'Gname',
+    title: "github名字",
+    dataIndex: "fullName",
   },
   {
-    title: '头像',
-    dataIndex: 'avatar',
+    title: "仓库名",
+    dataIndex: "name",
   },
   {
-    title: 'gitHub Address',
-    dataIndex: 'address',
+    title: "头像",
+    dataIndex: "avatar",
+  },
+  {
+    title: "gitHub Address",
+    dataIndex: "address",
   },
 ];
+const state = {
+  arr: [],
+};
+const data = [];
 
-const data = [
-  {
-    key: '1',
-    avatar: 'avatar-url',
-    Gname:'loginname',
-    address: 'New York No. 1 Lake Park',
-  },
-];
 
-function onChange(pagination, filters, sorter, extra) {
-  console.log('params', pagination, filters, sorter, extra);
-}
-function App(pagination, filters, sorter, extra) {
-  return (<Table columns={columns} dataSource={data} onChange={onChange} />)
-}
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const addName = () => {
+    setIsModalVisible(true);
+  };
+  const handleOk = (event) => {
+    axios.get(`https://api.github.com/users/${username}/repos`).then((resp) => {
+      console.log(resp.data);
+      state.arr.push(resp.data);
+      setUsername("");
+      setIsModalVisible(false);
+    });
+  };
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+  return (
+    <Fragment>
+      <Button type="primary" onClick={addName}>
+        添加
+      </Button>
+      <Modal
+        title="Basic Modal"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <form>
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            placeholder="GitHub username"
+            required
+          />
+        </form>
+      </Modal>
+      <Table columns={columns} dataSource={data} />
+    </Fragment>
+  );
+};
 
 export default App;
